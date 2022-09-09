3c253edf834bc310ed418d88e8de9b7aea8c5728
Max Tkachuk <max200325622@gmail.com>
2022-09-09 16:36:20 +0300

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,6 @@ class _MyAppState extends State<MyApp> {
 
   @override
   void initState() {
-    //SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
     _initSettings();
     super.initState();
   }
