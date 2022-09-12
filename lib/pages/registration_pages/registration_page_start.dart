26735b5dc7ab60fef842af1374c9f3638a60bd41
Max Tkachuk <max200325622@gmail.com>
2022-09-12 17:46:31 +0300

    new commit

--- lib/pages/registration_pages/registration_page_start.dart
+++ lib/pages/registration_pages/registration_page_start.dart
@@ -1,5 +1,6 @@
 import 'package:audiotales/generated/l10n.dart';
 import 'package:audiotales/resouses/colors.dart';
+import 'package:audiotales/resouses/fonts.dart';
 import 'package:audiotales/widgets/buttons/floating_actions_buttons.dart';
 import 'package:audiotales/widgets/paint/circular_wrapper.dart';
 import 'package:auto_size_text/auto_size_text.dart';
@@ -15,71 +16,57 @@ class RegistrationPageStart extends StatefulWidget {
 class _RegistrationPageStartState extends State<RegistrationPageStart> {
   @override
   Widget build(BuildContext context) {
-    //double m = MediaQuery.of(context).size.height;
     return Scaffold(
       body: CircularWrapper(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
-          //crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             // Expanded(
-            //   child:
-            //  Column(
-            //      children: [
-            //Padding(padding: EdgeInsets.only(top: m / 5)),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                AutoSizeText(
-                  S.of(context).splashScreenText,
-                  maxFontSize: 48.0,
-                  style: const TextStyle(
-                    color: white,
-                    fontWeight: FontWeight.w700,
-                    fontSize: 48.0,
-                    fontFamily: 'TTNorms',
+            //   flex: 1,
+            //   child: 
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  AutoSizeText(
+                    S.of(context).splashScreenText,
+                    maxFontSize: 48.0,
+                    style: const TextStyle(
+                      color: white,
+                      fontWeight: AppFonts.bold,
+                      fontSize: 48.0,
+                      fontFamily: AppFonts.fontFamily,
+                    ),
                   ),
-                ),
-                Text(
-                  S.of(context).underMemoryBox,
-                  style: const TextStyle(
-                    color: white,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 14.0,
-                    fontStyle: FontStyle.normal,
-                    fontFamily: 'TTNorms',
+                  AutoSizeText(
+                    S.of(context).underMemoryBox,
+                    style: const TextStyle(
+                      color: white,
+                      fontWeight: AppFonts.regular,
+                      fontSize: 14.0,
+                      fontFamily: AppFonts.fontFamily,
+                    ),
                   ),
-                ),
-              ],
-            ),
-
-            // FittedBox(
-            //   fit: BoxFit.scaleDown,
-            //   child: Padding(
-            //     padding: const EdgeInsets.only(left: 100.0),
-            //     child: Column(
-            //       children: [
-
-            //       ],
-            //     ),
-            //   ),
-            // ),
-            const SizedBox(height: 158.0),
-            Expanded(
-              child: Padding(
+                ],
+              ),
+           // ),
+            // Expanded(
+            //   flex: 2,
+            //   child: 
+              Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 50.0),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     Text(
                       S.of(context).hello,
                       textAlign: TextAlign.center,
                       style: const TextStyle(
                         color: black,
-                        fontWeight: FontWeight.w400,
+                        fontWeight: AppFonts.regular,
                         fontSize: 24.0,
-                        fontStyle: FontStyle.normal,
-                        fontFamily: 'TTNorms',
+                        fontFamily: AppFonts.fontFamily,
                       ),
                     ),
                     const SizedBox(height: 24.0),
@@ -88,10 +75,9 @@ class _RegistrationPageStartState extends State<RegistrationPageStart> {
                       textAlign: TextAlign.center,
                       style: const TextStyle(
                         color: black,
-                        fontWeight: FontWeight.w400,
+                        fontWeight: AppFonts.regular,
                         fontSize: 16.0,
-                        fontStyle: FontStyle.normal,
-                        fontFamily: 'TTNorms',
+                        fontFamily: AppFonts.fontFamily,
                       ),
                     ),
                     const SizedBox(height: 48.0),
@@ -99,12 +85,9 @@ class _RegistrationPageStartState extends State<RegistrationPageStart> {
                   ],
                 ),
               ),
-            ),
+          // ),
           ],
         ),
-        // ],
-        //  ),
-        // ),
       ),
     );
   }
